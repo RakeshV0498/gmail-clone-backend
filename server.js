ce5f8771e997c6379a5ab02d3230faff13b38d35
RakeshV0498 <rockr4674@gmail.com>
2024-06-22 12:00:49 +0530

    Implemented register route

--- server.js
+++ server.js
@@ -3,17 +3,20 @@ import cors from "cors";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 import mongoConnect from "./db-utils/mongoConnect.js";
+import registerRouter from "./routes/register.js";
+
+dotenv.config();
 
 const server = express();
-const port = 8100;
-
-dotenv.config;
+const port = process.env.PORT || "";
 
 server.use(express.json());
 
 server.use(cors());
 
 mongoConnect();
+
+server.use("/register", registerRouter);
 
 server.listen(port, () => {
   console.log(`${Date().toString()} - server listening on port ${port}`);
