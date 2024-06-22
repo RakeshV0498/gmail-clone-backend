ebdb3d123789b1c084252e75ebf3bca48fb8ad6e
RakeshV0498 <rockr4674@gmail.com>
2024-06-22 11:17:52 +0530

    Created Model for users

---
+++ db-utils/model.js
@@ -0,0 +1,37 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  dateOfBirth: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    uniqe: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date.now(),
+  },
+});
+
+const userModel = new model("User", userSchema, "Users");
+
+export { userModel };
