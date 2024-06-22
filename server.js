e213012cdfb4c1ae5d2c8d29f5b250a93bda7eab
RakeshV0498 <rockr4674@gmail.com>
2024-06-22 11:09:56 +0530

    linked mongoConnect

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
+import mongoConnect from "./db-utils/mongoConnect.js";
 
 const server = express();
 const port = 8100;
@@ -11,6 +12,8 @@ dotenv.config;
 server.use(express.json());
 
 server.use(cors());
+
+mongoConnect();
 
 server.listen(port, () => {
   console.log(`${Date().toString()} - server listening on port ${port}`);
