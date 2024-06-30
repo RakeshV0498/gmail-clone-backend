fff7f208eddbddf014e987c5e592e875df9ce1eb
RakeshV0498 <rockr4674@gmail.com>
2024-06-30 09:41:57 +0530

    Implemented email route logic

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import registerRouter from "./routes/register.js";
 import loginRouter from "./routes/login.js";
 import forgotPassRouter from "./routes/forgotPassword.js";
 import resetPassRouter from "./routes/resetPassword.js";
+import emailRouter from "./routes/email.js";
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ server.use("/register", registerRouter);
 server.use("/login", loginRouter);
 server.use("/forgot-password", forgotPassRouter);
 server.use("/reset-password", resetPassRouter);
+server.use("/emails", emailRouter);
 
 server.listen(port, () => {
   console.log(`${Date().toString()} - server listening on port ${port}`);
